8cbc05e7121a4271c2785b96b610efabf3c83e55
Isuru Bandaranayake <isbana.git@gmail.com>
2020-08-14 03:18:55 +0530

    Add folder structure

---
+++ frontend/src/components/PetSittersView/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class PetSittersView extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default PetSittersView
