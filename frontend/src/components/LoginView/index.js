7f4bbe9c7e0bf1f717480b8087ceac1717515041
Isuru Bandaranayake <isbana.git@gmail.com>
2020-09-22 03:03:31 +0530

    Add LoginView

--- frontend/src/components/LoginView/index.js
+++ frontend/src/components/LoginView/index.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
 
-export class index extends Component {
+export class LoginView extends Component {
     render() {
         return (
-            <div>
+            <div className='app'>
+                <div className="center-container">
+                    <Form className="bg-light p-5">
+                        <h3 className="text-center">Login</h3>
+                        <hr className="mb-5" />
 
+                        <FormGroup>
+                            <Label for="email">Email Address</Label>
+                            <Input type="email" name="email" id="email" placeholder="Enter your email address" />
+                        </FormGroup>
+
+                        <FormGroup>
+                            <Label for="password">Password</Label>
+                            <Input type="password" name="password" id="password" placeholder="Enter your password" />
+                        </FormGroup>
+
+                        <FormGroup className="mt-5">
+                            <Button className="btn-block">Login</Button>
+                        </FormGroup>
+
+                        <FormGroup className="text-center">
+                            <Label><a href="/reset">Forgotten Password?</a></Label>
+                        </FormGroup>
+                    </Form>
+                </div>
             </div>
         )
     }
 }
 
-export default index
+export default LoginView
