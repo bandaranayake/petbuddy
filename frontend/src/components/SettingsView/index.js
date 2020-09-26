32c3b16dfa65c75981d20d8a7298e00f8be442d0
Isuru Bandaranayake <isbana.git@gmail.com>
2020-09-26 13:32:45 +0530

    Add SettingsView

--- frontend/src/components/SettingsView/index.js
+++ frontend/src/components/SettingsView/index.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
 
 export class index extends Component {
     render() {
         return (
-            <div>
+            <div className="center-container">
+                <Form className="bg-light p-5">
+                    <h3 className="text-center">Account Settings</h3>
+                    <hr className="mb-5" />
 
+                    <FormGroup>
+                        <Label for="currpassword">Current Password</Label>
+                        <Input type="text" name="currpassword" id="currpassword" placeholder="Enter Current Password" />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="newpassword">New Password</Label>
+                        <Input type="text" name="newpassword" id="newpassword" placeholder="Enter New Password" />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Label for="renewpassword">Re-Enter New Password</Label>
+                        <Input type="text" name="renewpassword" id="renewpassword" placeholder="Re-enter New Password" />
+                    </FormGroup>
+
+                    <FormGroup className="mt-5">
+                        <Button className="btn-block">Proceed</Button>
+                    </FormGroup>
+                </Form>
             </div>
         )
     }
