82763e67b52c999f1016f07c0cca6ba6fd5c9a2b
Isuru Bandaranayake <isbana.git@gmail.com>
2020-09-26 19:14:09 +0530

    Add 404 Page

--- frontend/src/components/ErrorPage/index.js
+++ frontend/src/components/ErrorPage/index.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
-export class index extends Component {
+import * as ROUTES from '../../constants/routes';
+
+export class ErrorPage extends Component {
     render() {
         return (
-            <div>
-
-            </div>
-        )
+            <div className='app'>
+                <div className="text-center" style={{ 'margin': 'auto' }}>
+                    <h1 className="text-primary display-1">404</h1>
+                    <h2 className="offset-md text-center mb-5">We couldn't find the page <br /> you are looking for (x_x)</h2>
+                    <Link className="btn btn-primary" to={ROUTES.HOME}>Back to Home</Link>
+                </div>
+            </div >
+        );
     }
 }
 
-export default index
+export default ErrorPage
