914910afcc1ac68664a03ae73ea51d9ee007623c
Isuru Bandaranayake <isbana.git@gmail.com>
2020-10-24 21:08:23 +0530

    Add ServicesView

--- frontend/src/containers/ServicesView/index.js
+++ frontend/src/containers/ServicesView/index.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import { Table, Row, Input } from 'reactstrap'
+import { FaSave, FaBan } from 'react-icons/fa';
 
 export class ServicesView extends Component {
     render() {
         return (
-            <div>
-
-            </div>
+            <main className="px-5">
+                <Row className="py-4">
+                    <Table responsive hover bordered>
+                        <thead>
+                            <tr>
+                                <th width="85%">Service Name</th>
+                                <th width="15%">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td> <Input type="text" value="Pet Sitting" /></td>
+                                <td>
+                                    <div style={{ textAlign: 'center' }}>
+                                        <Link className="btn btn-primary btn-md table-actions-btn">
+                                            <FaSave />
+                                        </Link>
+                                        <Link className="btn btn-danger btn-md table-actions-btn">
+                                            <FaBan />
+                                        </Link>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </Row>
+            </main >
         )
     }
 }
