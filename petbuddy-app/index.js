ed6f67ff6f8859dab184491dea9944ba1104f070
Isuru Bandaranayake <isbana.git@gmail.com>
2020-11-10 23:46:37 +0530

    Add theme

--- petbuddy-app/index.js
+++ petbuddy-app/index.js
@@ -2,9 +2,20 @@
  * @format
  */
 
+import * as React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { name as appName } from './app.json';
+import App from './App';
+import { theme } from './src/core/theme';
 import 'react-native-gesture-handler';
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+    return (
+        <PaperProvider theme={theme}>
+            <App />
+        </PaperProvider>
+    );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
---
+++ petbuddy-app/src/core/theme/index.js
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'react-native-paper';
+
+export const theme = {
+    ...DefaultTheme,
+    roundness: 2,
+    colors: {
+        ...DefaultTheme.colors,
+        background: 'white',
+        primary: '#1765f5',
+        secondary: '#bbbbbb',
+    },
+};
