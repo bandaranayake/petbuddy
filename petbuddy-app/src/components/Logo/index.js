90efdfba4578b9a0995614714d3146f2a0a5f1ae
Isuru Bandaranayake <isbana.git@gmail.com>
2020-11-11 06:10:17 +0530

    Add logo component

---
+++ petbuddy-app/src/components/Logo/index.js
@@ -0,0 +1,30 @@
+import React, { memo } from 'react';
+import { View, Image, Text, StyleSheet } from 'react-native';
+import { theme } from '../../core/theme';
+
+const Logo = () => (
+    <View style={styles.container}>
+        <Image source={require('../../assets/logo.png')} style={styles.image} />
+        <Text style={styles.logo}>Pet Buddy</Text>
+    </View>
+);
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        width: 100,
+        height: 100,
+        marginBottom: 12,
+    },
+    logo: {
+        color: theme.colors.secondary,
+        fontSize: 20,
+        fontWeight: 'bold',
+        paddingVertical: 0,
+    }
+});
+
+export default memo(Logo);
