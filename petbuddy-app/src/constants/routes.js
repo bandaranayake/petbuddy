a91745af41ddb2a8087cb01d8fca330c12abca39
Isuru Bandaranayake <isbana.git@gmail.com>
2020-12-08 20:31:29 +0530

    Add routes constants

---
+++ petbuddy-app/src/constants/routes.js
@@ -0,0 +1,12 @@
+export const FILTER = 'Filter';
+export const LANDING = 'Landing';
+export const LOGIN = 'Login';
+export const MAIN = 'Main';
+export const RESET = 'Reset';
+export const SERVICE = 'Service';
+export const SIGNUP = 'Signup';
+export const SIGNUP2 = 'Signup2';
+export const BOOKING = 'Booking';
+export const CHAT = 'Chat';
+export const MAKE_BOOKING = 'MakeBooking';
+export const SETTINGS = 'Settings';
