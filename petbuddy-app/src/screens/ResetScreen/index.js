2208be3bc435540c22a3e9fdac388f02129e0f72
Isuru Bandaranayake <isbana.git@gmail.com>
2020-12-09 05:59:15 +0530

    Add firebase password reset

--- petbuddy-app/src/screens/ResetScreen/index.js
+++ petbuddy-app/src/screens/ResetScreen/index.js
@@ -1,12 +1,46 @@
-import React, { memo } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Title, TextInput } from 'react-native-paper';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button as PaperButton, Dialog, Portal, Title, TextInput } from 'react-native-paper';
+import auth from '@react-native-firebase/auth';
 import { theme } from '../../core/theme';
+import { ValidateEmail } from '../../utils/validation';
+import * as ROUTES from '../../constants/routes'
 import Background from '../../components/Background';
 import Logo from '../../components/Logo';
 import Button from '../../components/Button';
 
-function ResetScreen() {
+function ResetScreen({ navigation }) {
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState('');
+    const [visible, setVisible] = useState(false);
+
+    const showDialog = () => setVisible(true);
+    const hideDialog = () => setVisible(false);
+
+    function handleReset() {
+        if (!ValidateEmail(email)) {
+            setError('The provided email address is not valid. Please enter a valid email address.');
+            showDialog();
+        }
+        else {
+            auth()
+                .sendPasswordResetEmail(email)
+                .then(() => navigation.navigate(ROUTES.LOGIN))
+                .catch(error => {
+                    if (error.code === 'auth/user-not-found') {
+                        setError('There is no user record corresponding to the provided email address.');
+                    }
+                    else if (error.code === 'auth/invalid-email') {
+                        setError('That provided email address is invalid. Please enter a valid email address.');
+                    }
+                    else {
+                        setError('Something went wrong. Please try again later.');
+                    }
+                    showDialog();
+                });
+        }
+    }
+
     return (
         <Background>
             <View style={styles.logo} >
@@ -14,9 +48,20 @@ function ResetScreen() {
             </View>
             <View style={styles.form}>
                 <Title style={styles.title}>Reset Password</Title>
-                <TextInput mode='flat' label='Email' placeholder='Your email address' style={styles.input} />
-                <Button mode='contained' style={styles.button}>Continue</Button>
+                <TextInput mode='flat' label='Email' placeholder='Your email address' style={styles.input} onChangeText={(text) => setEmail(text)} />
+                <Button mode='contained' style={styles.button} onPress={() => handleReset()}>Continue</Button>
             </View>
+            <Portal>
+                <Dialog visible={visible} onDismiss={hideDialog}>
+                    <Dialog.Title>Password Reset Error</Dialog.Title>
+                    <Dialog.Content>
+                        <Text>{error}</Text>
+                    </Dialog.Content>
+                    <Dialog.Actions>
+                        <PaperButton onPress={hideDialog}>Ok</PaperButton>
+                    </Dialog.Actions>
+                </Dialog>
+            </Portal>
         </Background>
     );
 }
@@ -44,4 +89,4 @@ const styles = StyleSheet.create({
     },
 });
 
-export default memo(ResetScreen);
+export default ResetScreen;
