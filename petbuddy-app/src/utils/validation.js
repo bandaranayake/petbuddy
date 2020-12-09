b5ce9ccbfdf19d8cb452ac113a38af5daa9ab488
Isuru Bandaranayake <isbana.git@gmail.com>
2020-12-09 06:01:11 +0530

    Add validations

---
+++ petbuddy-app/src/utils/validation.js
@@ -0,0 +1,8 @@
+export const ValidateEmail = email => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+export const ValidatePhone = phone => {
+    return phone.match(/\d/g).length === 10;
+}
