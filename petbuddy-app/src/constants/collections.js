42ce4d472980c9c5c32cc8b1564f2357a2cc0ac5
Isuru Bandaranayake <isbana.git@gmail.com>
2020-12-11 12:47:45 +0530

    Add constants

---
+++ petbuddy-app/src/constants/collections.js
@@ -0,0 +1,5 @@
+export const PROFILES = 'profiles';
+export const SERVICES = 'services';
+export const PETS = 'pets';
+export const BOOKINGS = 'bookings';
+export const MESSAGES = 'messages';
---
+++ petbuddy-app/src/constants/global.js
@@ -0,0 +1,10 @@
+export const PETS = ['Dogs', 'Cats', 'Rabbits', 'Birds', 'Other'];
+export const SERVICES = ['House sitting', 'Pet boarding', 'Dog walking', 'Pet grooming'];
+export const PREFERENCES = ['Lives in an apartment', 'Have kids', 'Own a cat', 'Own a dog'];
+export const LEVELS = ['Level 1', 'Level 2', 'Level 3', 'Level 4', 'Level 5'];
+export const DISTRICTS = ['Ampara', 'Ampara', 'Anuradhapura', 'Anuradhapura', 'Badulla', 'Badulla',
+    'Batticaloa', 'Batticaloa', 'Colombo', 'Colombo', 'Galle', 'Galle', 'Gampaha', 'Gampaha', 'Hambantota',
+    'Hambantota', 'Jaffna', 'Jaffna', 'Kalutara', 'Kalutara', 'Kandy', 'Kandy', 'Kegalle', 'Kegalle',
+    'Kilinochchi', 'Kilinochchi', 'Kurunegala', 'Kurunegala', 'Mannar', 'Mannar', 'Matale', 'Matale',
+    'Matara', 'Matara', 'Monaragala', 'Monaragala', 'Nuwara Eliya', 'NEliya', 'Polonnaruwa', 'Polonnaruwa',
+    'Puttalam', 'Puttalam', 'Ratnapura', 'Ratnapura', 'Trincomalee', 'Trincomalee', 'Vavuniya', 'Vavuniya'];
