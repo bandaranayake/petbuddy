7a819280c18da87c54c02adcb2b74db493bfa936
Isuru Bandaranayake <isbana.git@gmail.com>
2020-12-12 05:33:16 +0530

    Add background image

--- petbuddy-app/src/screens/LandingScreen/index.js
+++ petbuddy-app/src/screens/LandingScreen/index.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import * as ROUTES from '../../constants/routes'
-import Background from '../../components/Background';
 import Button from '../../components/Button';
+import { theme } from '../../core/theme';
 
 function LandingScreen({ navigation }) {
     return (
-        <Background>
-            <View>
-                <Button mode='contained' onPress={() => navigation.navigate(ROUTES.FILTER)}>Find Pet Sitter</Button>
+        <ImageBackground source={require('../../assets/background.png')} style={styles.container}>
+            <View style={styles.inner}>
+                <Text style={styles.title}>PET BUDDY</Text>
+                <Button mode='contained' onPress={() => navigation.navigate(ROUTES.MAIN)}>Find Pet Sitter</Button>
                 <Button mode='contained' onPress={() => navigation.navigate(ROUTES.SIGNUP)}>Sign Up</Button>
                 <Button mode='contained' onPress={() => navigation.navigate(ROUTES.LOGIN)}>Sign In</Button>
             </View>
-        </Background>
+        </ImageBackground>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+    },
+    inner: {
+        flex: 1,
+        padding: 45,
+        marginBottom: 30,
+        justifyContent: 'flex-end',
+    },
+    title: {
+        alignSelf: 'center',
+        marginBottom: 20,
+        fontSize: 35,
+        fontWeight: 'bold',
+        color: theme.colors.primary,
+        textShadowColor: 'rgba(0, 0, 0, 0.25)',
+        textShadowOffset: { width: -1, height: 1 },
+        textShadowRadius: 20,
+    },
+});
 
 export default LandingScreen;
