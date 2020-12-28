fe544fdec6c54afc449ef5c3c77bb7fc284edd39
Isuru Bandaranayake <isbana.git@gmail.com>
2020-12-28 22:19:28 +0530

    Add password reset

--- petbuddy-app/src/screens/SettingsScreen/index.js
+++ petbuddy-app/src/screens/SettingsScreen/index.js
@@ -1,20 +1,59 @@
-import React, { memo } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Title, TextInput } from 'react-native-paper';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button as PaperButton, Dialog, Portal, Title, TextInput } from 'react-native-paper';
+import auth from '@react-native-firebase/auth';
 import { theme } from '../../core/theme';
 import Background from '../../components/Background';
 import Button from '../../components/Button';
 
-function SettingsScreen({ navigation }) {
+function SettingsScreen() {
+    const [password, setPassword] = useState('');
+    const [cPassword, setCPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const [visible, setVisible] = useState(false);
+
+    const showDialog = () => setVisible(true);
+    const hideDialog = () => setVisible(false);
+
+    const updatePassword = () => {
+        if (password.length < 8) {
+            setMessage('Password must be of minimum 8 characters length.');
+            showDialog();
+        }
+        else if (cPassword !== password) {
+            setMessage('Passwords does not match.');
+            showDialog();
+        }
+        else {
+            auth().currentUser.updatePassword(password).then(() => {
+                setMessage('Password updated successfully.');
+                showDialog();
+            }).catch(err => {
+                setMessage('Something went wrong. Please try again later.');
+                showDialog();
+            });
+        }
+    }
+
     return (
         <Background>
             <View>
                 <Title style={styles.title}>Settings</Title>
-                <TextInput mode='flat' label='Current Password' placeholder='Enter current password' style={styles.input} secureTextEntry />
-                <TextInput mode='flat' label='New Password' placeholder='Enter new password' style={styles.input} secureTextEntry secureTextEntry />
-                <TextInput mode='flat' label='Confirm Password' placeholder='Confirm new password' style={styles.input} secureTextEntry secureTextEntry />
-                <Button mode='contained' style={styles.button}>Update Password</Button>
+                <TextInput mode='flat' label='New Password' placeholder='Enter new password' style={styles.input} secureTextEntry secureTextEntry onChangeText={(text) => setPassword(text.trim())} />
+                <TextInput mode='flat' label='Confirm Password' placeholder='Confirm new password' style={styles.input} secureTextEntry secureTextEntry onChangeText={(text) => setCPassword(text.trim())} />
+                <Button mode='contained' style={{ marginTop: 25 }} onPress={() => updatePassword()}>Update Password</Button>
             </View>
+            <Portal>
+                <Dialog visible={visible} onDismiss={hideDialog}>
+                    <Dialog.Title>Update Password</Dialog.Title>
+                    <Dialog.Content>
+                        <Text>{message}</Text>
+                    </Dialog.Content>
+                    <Dialog.Actions>
+                        <PaperButton onPress={hideDialog}>Ok</PaperButton>
+                    </Dialog.Actions>
+                </Dialog>
+            </Portal>
         </Background>
     );
 }
@@ -29,9 +68,6 @@ const styles = StyleSheet.create({
         backgroundColor: theme.colors.background,
         marginTop: 16,
     },
-    button: {
-        marginTop: 25,
-    },
 });
 
-export default memo(SettingsScreen);
+export default SettingsScreen;
