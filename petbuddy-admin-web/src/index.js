17d8d89c83cdedeec74daef435510f6846884db9
Isuru Bandaranayake <isbana.git@gmail.com>
2021-01-26 11:07:54 +0530

    Add redux

--- petbuddy-admin-web/src/index.js
+++ petbuddy-admin-web/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider as ReduxProvider } from 'react-redux';
 import App from './App';
+import store from './store';
 import * as serviceWorker from './serviceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
