4a6539114bef11c96ba08b5148249be1499b5220
Isuru Bandaranayake <isbana.git@gmail.com>
2021-05-08 16:51:51 +0530

    Add constants

--- petbuddy-admin-web/src/constants/routes.js
+++ petbuddy-admin-web/src/constants/routes.js
@@ -1,9 +1,8 @@
 export const HOME = '/';
 export const RESET = '/reset';
-export const DASHBOARD = '/dashboard';
+export const DASHBOARD = '/';
 export const CUSTOMERS = '/customers';
 export const PETSITTERS = '/petsitters';
 export const SERVICES = '/services';
 export const BOOKINGS = '/bookings';
 export const SETTINGS = '/settings';
-export const LOGOUT = '/logout';
