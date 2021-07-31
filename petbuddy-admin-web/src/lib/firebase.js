56bc820c3b9617cfc85e281b8a47c9e9e6aa31f3
Isuru Bandaranayake <isbana.git@gmail.com>
2021-07-31 13:22:00 +0530

    Add env file

--- petbuddy-admin-web/src/lib/firebase.js
+++ petbuddy-admin-web/src/lib/firebase.js
@@ -3,14 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_ID,
+    measurementId: process.env.REACT_MEASUREMENT_ID
 };
 
 firebase.initializeApp(config);
